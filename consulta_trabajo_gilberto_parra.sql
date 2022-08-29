fcc000194574007b95705e43fd45d4d72f3ada49
Gil_Parra <gilberto.parra@dgru.unam.mx>
2022-08-29 11:42:13 -0500

    modificaciones

--- consulta_trabajo_gilberto_parra.sql
+++ consulta_trabajo_gilberto_parra.sql
@@ -1,1 +1,17 @@
 --Esto es un comentario
+
+/*
+pasos 
+1.- Abrir Git Bash
+2.- Clonar el repositorio
+3.- Abrir la carpeta del repositorio con Git Bash
+4.- Escirbir en Git Bash git init
+5.- Crear un nuevo archivo
+6.- Escribir git status
+7.- Escibir git add y el nombre del archivo creado en el paso 5
+8.- Poner las credenciales de Git config (git config --global user.email "nombre@"
+git config --global user.name "Mi nombre de usuario")
+8.- Escribir
+9.- Escribir git commit -m "Nombre del comit(aqui va la descripcion del archivo, p.ej."primera consulta de gil"")
+10.- Escribir git push origin main (aqui va a pedir su nombre de usuario y token de Git Hub)
+*/
